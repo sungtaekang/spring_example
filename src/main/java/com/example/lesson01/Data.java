efa3577e9af82c36c08c1411052903abd2a9b3b9
test <test@test.com>
2023-06-14 17:06:54 +0900

    초기 환경 세팅

---
+++ src/main/java/com/example/lesson01/Data.java
@@ -0,0 +1,26 @@
+package com.example.lesson01;
+
+// 일반 JAVA bean
+public class Data {
+	// field
+	private int id;
+	private String name;
+	
+	
+	// getter, setter
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
